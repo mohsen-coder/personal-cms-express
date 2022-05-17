411cbb7308f207ea3422097731e6f41d1de3ec11
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 18:01:06 +0430

    update(LoginPostValidation.ts): use redirectWithMessage util to redirect user

--- middlewares/LoginPostValidation.ts
+++ middlewares/LoginPostValidation.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
+import { RouteHelper } from '../helper/RouteHelper';
+import { redirectWithMessage } from '../utils/RedirectUtil';
 import { loginPostSchema } from "../validators/LoginPostValidator"
 
 export const LoginPostValidation = (req: Request, res: Response, next: NextFunction) => {
     const { error } = loginPostSchema.validate(req.body, { abortEarly: false })
-    if (error) return res.redirect(`/login?error-message="${error.message}"`);
+    if (error) return res.redirect(redirectWithMessage(RouteHelper.login, [error.message], "error"));
     next()
 }
