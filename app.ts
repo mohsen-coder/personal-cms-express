f35fe56ade7016aa127e644ffb2c5cbc63879bfd
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 14:25:32 +0430

    update(app.ts): add template engine, add pulic folder as static resource, add body parser, add session, sync database and tables

--- app.ts
+++ app.ts
@@ -1,12 +1,36 @@
+import path from 'path';
 import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser';
+import mustacheExpress from 'mustache-express';
+import session from 'express-session'
+import connectSequelize from 'connect-session-sequelize'
+import {database, DBSyncer} from './config/Database'
 
 
 (async () => {
     const app = express()
+    const SequelizeStore = connectSequelize(session.Store);
 
-    app.get('/', (req: Request, res: Response) => {
-        res.send('<h1> Hello World </h1>')
-    })
 
-    app.listen(8000, () => console.log('server is runing on http://localhost:8000'))
+    app.engine('mustache', mustacheExpress());
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'mustache');
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(session({
+        secret: 'session key id',
+        store: new SequelizeStore({ db: database }),
+        resave: false,
+        saveUninitialized: false
+    }))
+
+
+    try {
+        await DBSyncer()
+        app.listen(8000, () => console.log('server is runing on http://localhost:8000'))
+    } catch (err) {
+        console.log(err);
+        console.log("fail to start server");
+    }
+
 })()
