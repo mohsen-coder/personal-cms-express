7179e4f5eb1c60e2ddb94911d3e46bb8c0c5675e
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 18:00:32 +0430

    update(RegisterPostValidation.ts): use redirectWithMessage util to redirect user

--- middlewares/RegisterPostValidation.ts
+++ middlewares/RegisterPostValidation.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
+import { RouteHelper } from '../helper/RouteHelper';
+import { redirectWithMessage } from '../utils/RedirectUtil';
 import { registerPostSchema } from "../validators/RegisterPostValidator";
 
 export const RegisterPostValidation = async (req: Request, res: Response, next: NextFunction) => {
     const { error } = registerPostSchema.validate(req.body, { abortEarly: false })
-    if (error) return res.redirect(`/register?error-message="${error.message}"`);
+    if (error) return res.redirect(redirectWithMessage(RouteHelper.register, [error.message], "error"));
     next()
 }
