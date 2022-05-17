919c128d13fe607112ab304281a4687743ff6117
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 16:23:43 +0430

    update(RegisterPost.ts): add request, controller, response and redirect user

--- middlewares/RegisterPost.ts
+++ middlewares/RegisterPost.ts
@@ -1,5 +1,16 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
+import { RegisterController } from '../controllers/RegisterController'
+import { RouteHelper } from '../helper/RouteHelper'
+import { UserModel } from '../models/UserModel'
+import { RegisterRequest } from '../request/RegisterRequest'
+import { redirectWithMessage } from '../utils/RedirectUtil'
 
-export const RegisterPost = (req: Request, res: Response) => {
+export const RegisterPost = async (req: Request, res: Response) => {
+    const registerRequest = new RegisterRequest(req.body)
+    const controller = new RegisterController(registerRequest, UserModel)
+    const response = await controller.registerUser()
+    if (response.status === "error") 
+        return res.redirect(redirectWithMessage(RouteHelper.register, response.messages, response.status));
     
+    return res.redirect(RouteHelper.login)
 }
