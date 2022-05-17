9fac466fd3ca25cb58ed8b3f70c00ee76dcac33d
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 14:36:53 +0430

    controllers/LoginController.ts

---
+++ controllers/LoginController.ts
@@ -0,0 +1,39 @@
+import { UserModel } from "../models/UserModel";
+import { LoginRequest } from "../request/LoginRequest";
+import { ResponseBase } from "../response/ResponseBase";
+import { ResponseStatus } from "../response/ResponseStatus";
+import { HashUtil } from "../utils/HashUtil";
+
+export class LoginController {
+
+    private request: LoginRequest
+    private model: typeof UserModel
+
+    constructor(request: LoginRequest, model: typeof UserModel) {
+        this.request = request
+        this.model = model
+    }
+
+    async loginUser(): Promise<ResponseBase> {
+        const response = new ResponseBase()
+
+        const user = await this.model.getUserByEmail(this.request.email)
+        if (user === null) {
+            response.messages.push('شما هنوز ثبت نام نکردید!')
+            response.status = ResponseStatus.error
+            return response
+        }
+
+        if (!await HashUtil.comparePassword(this.request.password, user.password)) {
+            response.messages.push('اطلاعات وارد شده صحیح نیست!')
+            response.status = ResponseStatus.error
+            return response
+        }
+
+        response.messages.push('با موفقیت وارد شدید!')
+        response.status = ResponseStatus.success
+
+        return response
+    }
+
+}
