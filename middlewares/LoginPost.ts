134c1c2f5e62f68121b180a46c0bcf8304e754b3
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 18:17:00 +0430

    update(LoginPost.ts): add request, controller, response and redirect user

--- middlewares/LoginPost.ts
+++ middlewares/LoginPost.ts
@@ -1,5 +1,17 @@
 import {Request, Response} from 'express'
+import { LoginController } from '../controllers/LoginController'
+import { RouteHelper } from '../helper/RouteHelper'
+import { UserModel } from '../models/UserModel'
+import { LoginRequest } from '../request/LoginRequest'
+import { redirectWithMessage } from '../utils/RedirectUtil'
 
-export const LoginPost = (req: Request, res: Response) => {
+export const LoginPost = async (req: Request, res: Response) => {
+    const loginRequest = new LoginRequest(req.body, req.session)
+    const controller = new LoginController(loginRequest, UserModel)
+    const response = await controller.loginUser()
+
+    if(response.status === "error") 
+        return res.redirect(redirectWithMessage(RouteHelper.login, response.messages, response.status))
     
+    res.redirect(RouteHelper.panel)
 }
