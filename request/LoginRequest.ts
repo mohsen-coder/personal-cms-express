30f23f4aad0974d64d9d02f65c293cb083df629b
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 15:33:12 +0430

    update(LoginRequest.ts): add session field

--- request/LoginRequest.ts
+++ request/LoginRequest.ts
@@ -1,10 +1,13 @@
+import {Session, SessionData} from "express-session"
 export class LoginRequest {
 
     email: string = ''
     password: string = ''
+    session: Session & Partial<SessionData>
 
-    constructor(init: object) {
+    constructor(init: object, session: Session & Partial<SessionData>) {
         Object.assign(this, init)
+        this.session = session
     }
     
 }
