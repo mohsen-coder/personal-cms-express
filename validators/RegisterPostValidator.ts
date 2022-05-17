89caab360a835d907c818e71d94159a6b648fa39
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 17:56:11 +0430

    update(RegisterPostValidator.ts): rename firstName and lastName to name and family, add string.empty error message to name, family and email props

--- validators/RegisterPostValidator.ts
+++ validators/RegisterPostValidator.ts
@@ -2,20 +2,23 @@ import Joi from "joi";
 
 const schema = Joi.object({
 
-    firstName: Joi.string().trim().min(3).max(25).required().messages({
+    name: Joi.string().trim().min(3).max(25).required().messages({
         'string.min': 'نام حداقل باید ۳ حرف باشد!',
         'string.max': 'نام حداکثر باید 25 حرف باشد!',
+        'string.empty': 'نام نمی تواند خالی باشد!',
         'any.required': 'وارد کردن نام اجباری می باشد!'
     }),
 
-    lastName: Joi.string().trim().min(2).max(25).required().messages({
+    family: Joi.string().trim().min(2).max(25).required().messages({
         'string.min': 'نام خانوادگی حداقل باید ۳ حرف باشد!',
         'string.max': 'نام خانوادگی حداکثر باید 25 حرف باشد!',
+        'string.empty': 'نام خانوادگی نمی تواند خالی باشد!',
         'any.required': 'وارد کردن نام خانوادگی اجباری می باشد!'
     }),
 
     email: Joi.string().email().required().messages({
         'string.email': 'لطفا یک ایمیل معتبر وارد کنید!',
+        'string.empty': 'ایمیل نمی تواند خالی باشد!',
         'any.required': 'وارد کردن ایمیل اجباری می باشد!'
     }),
 
