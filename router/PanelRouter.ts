0ec571cdb82722f40faea92e92293cac6d8c4afe
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-17 21:40:31 +0430

    update(PanelRouter.ts): add category routes

--- router/PanelRouter.ts
+++ router/PanelRouter.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { RouteHelper } from "../helper/RouteHelper";
+import { CategoryDelete } from "../middlewares/panel/CategoryDelete";
+import { CategoryGet } from "../middlewares/panel/CategoryGet";
+import { CategoryPost } from "../middlewares/panel/CategoryPost";
+import { CategoryPut } from "../middlewares/panel/CategoryPut";
 import { DashboardGet } from "../middlewares/panel/DashboardGet";
 
 const panel = Router()
 
 panel.get(RouteHelper.panel, DashboardGet)
+
+panel.get(RouteHelper.categories, CategoryGet)
+panel.post(RouteHelper.categories, CategoryPost)
+panel.put(RouteHelper.categories, CategoryPut)
+panel.delete(RouteHelper.categories, CategoryDelete)
 
 export { panel as panelRouter }
