b3721fedab44f98273081af030700ed7d01b08d9
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-19 16:39:31 +0430

    update(PanelRouter.ts): add validation middlewares to categories routes

--- router/PanelRouter.ts
+++ router/PanelRouter.ts
@@ -1,18 +1,22 @@
-import { Router } from "express";
-import { RouteHelper } from "../helper/RouteHelper";
-import { CategoryDelete } from "../middlewares/panel/CategoryDelete";
-import { CategoryGet } from "../middlewares/panel/CategoryGet";
-import { CategoryPost } from "../middlewares/panel/CategoryPost";
-import { CategoryPut } from "../middlewares/panel/CategoryPut";
-import { DashboardGet } from "../middlewares/panel/DashboardGet";
+import {Router} from "express";
+import {RouteHelper} from "../helper/RouteHelper";
+import {CategoryDelete} from "../middlewares/panel/CategoryDelete";
+import {CategoryGet} from "../middlewares/panel/CategoryGet";
+import {CategoryPost} from "../middlewares/panel/CategoryPost";
+import {CategoryPut} from "../middlewares/panel/CategoryPut";
+import {DashboardGet} from "../middlewares/panel/DashboardGet";
+import {CategoryPostValidation} from "../middlewares/panel/CategoryPostValidation";
+import {CategoryGetValidation} from "../middlewares/panel/CategoryGetValidation";
+import {CategoryPutValidation} from "../middlewares/panel/CategoryPutValidation";
+import {CategoryDeleteValidation} from "../middlewares/panel/CategoryDeleteValidation";
 
 const panel = Router()
 
 panel.get(RouteHelper.panel, DashboardGet)
 
-panel.get(RouteHelper.categories, CategoryGet)
-panel.post(RouteHelper.categories, CategoryPost)
-panel.put(RouteHelper.categories, CategoryPut)
-panel.delete(RouteHelper.categories, CategoryDelete)
+panel.get(RouteHelper.categories, CategoryGetValidation, CategoryGet)
+panel.post(RouteHelper.categories, CategoryPostValidation, CategoryPost)
+panel.put(RouteHelper.categories, CategoryPutValidation, CategoryPut)
+panel.delete(RouteHelper.categories, CategoryDeleteValidation, CategoryDelete)
 
-export { panel as panelRouter }
+export {panel as panelRouter}
