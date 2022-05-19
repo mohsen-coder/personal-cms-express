4780696fb0bc64a731b48ad6758081c0664bfed4
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-19 16:35:06 +0430

    update(Database.ts): add CategoryModel sync method

--- config/Database.ts
+++ config/Database.ts
@@ -1,8 +1,10 @@
 import { database } from "./DB";
 import { UserModel } from "../models/UserModel";
+import {CategoryModel} from "../models/CategoryModel";
 
 async function DBSyncer(){
     await UserModel.sync()
+    await CategoryModel.sync()
 }
 
 export {database, UserModel, DBSyncer}
