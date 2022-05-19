9a8ea06c286fc45b814574b78769dd6fb5ca1596
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-19 16:36:10 +0430

    update(CategoryGet.ts): add request, controller and response. pass data to view

--- middlewares/panel/CategoryGet.ts
+++ middlewares/panel/CategoryGet.ts
@@ -1,5 +1,11 @@
 import {Request, Response} from 'express'
+import {CategoryGetRequest} from "../../request/CategoryGetRequest";
+import {CategoryController} from "../../controllers/CategoryController";
+import {CategoryModel} from "../../models/CategoryModel";
 
-export const CategoryGet = (req: Request, res: Response) => {
-    res.render('panel/categories', {pageTitle: 'دسته بندی ها', categories: []})
+export const CategoryGet = async (req: Request, res: Response) => {
+    const categoryGetRequest = new CategoryGetRequest();
+    const categoryController = new CategoryController(CategoryModel);
+    const responseBase = await categoryController.getCategory(categoryGetRequest);
+    res.render('panel/categories', {pageTitle: 'دسته بندی ها', ...responseBase.data})
 }
