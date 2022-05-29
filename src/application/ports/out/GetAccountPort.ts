6b0c1a4198b10a3fb30c40aa7a2bae9bbe08b59c
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-05-29 12:20:31 +0430

    update(GetAccountPort.ts): add getAccountByUsername and getAccounts methods

--- src/application/ports/out/GetAccountPort.ts
+++ src/application/ports/out/GetAccountPort.ts
@@ -3,5 +3,7 @@ import {AccountDAO} from "./dao/AccountDAO";
 export interface GetAccountPort {
     getAccountById(id: string): Promise<AccountDAO | null>
     getAccountByEmail(email: string): Promise<AccountDAO | null>
+    getAccountByUsername(username: string): Promise<AccountDAO | null>
     getAccountByRole(role: string): Promise<AccountDAO[]>
+    getAccounts(offset: number, limit: number): Promise<AccountDAO[]>
 }
