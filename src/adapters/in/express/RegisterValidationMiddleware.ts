2c02eea75d55610f6c83bbd067f32d1ccb76ce68
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-13 10:19:35 +0430

    refactor(RegisterValidationMiddleware.ts): refactor validation schema

--- src/adapters/in/express/RegisterValidationMiddleware.ts
+++ src/adapters/in/express/RegisterValidationMiddleware.ts
@@ -1,26 +1,50 @@
 import {NextFunction, Request, Response} from "express";
 import Joi from "joi";
+import {ResponseBase} from "../../../application/ports/in/response/ResponseBase";
+import {ResponseStatus} from "../../../application/ports/in/response/ResponseStatus";
 
 export function RegisterValidationMiddleware(){
 
     const schema = Joi.object({
-        name: Joi.string().trim().min(2).max(25).required(),
-        family: Joi.string().trim().min(2).max(25).required(),
-        email: Joi.string().trim().email().required(),
-        username: Joi.string().trim().min(3).max(25).required(),
-        password: Joi.string().trim().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
-        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+        name: Joi.string().trim().min(2).max(25).required().messages({
+            'string.min': 'نام باید حداقل ۲ حرف باشد!',
+            'string.max': 'نام نمی تواند بیش از ۲۵ حرف باشد!',
+            'any.required': 'نام الزامی می باشد!'
+        }),
+        family: Joi.string().trim().min(2).max(25).required().messages({
+            'string.min': 'نام خانوادگی باید حداقل ۲ حرف باشد!',
+            'string.max': 'نام خانوادگی نمی تواند بیش از ۲۵ حرف باشد!',
+            'any.required': 'نام خانوادگی الزامی می باشد!'
+        }),
+        email: Joi.string().trim().email().required().messages({
+            'string.email': 'ایمیل معتبر نمی باشد!',
+            'any.required': 'ایمیل الزامی می باشد!'
+        }),
+        username: Joi.string().trim().min(3).max(25).required().messages({
+            'string.min': 'نام کاربری باید حداقل ۳ حرف باشد!',
+            'string.max': 'نام کاربری نمی تواند بیش از ۲۵ حرف باشد!',
+            'any.required': 'نام کاربری الزامی می باشد!'
+        }),
+        password: Joi.string().trim().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).required().messages({
+            'string.pattern.base': 'گذرواژه قابل پذیرش نیست!',
+            'any.required': 'گذرواژه الزامی می باشد!'
+        }),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+            'any.only': 'تکرار گذرواژه صحیح نیست!',
+            'any.required': 'تکرار گذرواژه الزامی می باشد!'
+        })
     })
 
-    return (req: Request, res: Response, next: NextFunction) => {
-        const {error} = schema.validate(req.body, {abortEarly: false})
-
+    return (request: Request, response: Response, next: NextFunction) => {
+        const {error} = schema.validate(request.body, {abortEarly: false})
+        console.log(error)
         if (error) {
-            res.status(400)
-            res.send(error)
+            const responseBase = new ResponseBase();
+            responseBase.status = ResponseStatus.error;
+            responseBase.messages = error.details.map(err => err.message)
+            response.status(400).send(responseBase);
             return;
         }
-
         next()
     }
 }
