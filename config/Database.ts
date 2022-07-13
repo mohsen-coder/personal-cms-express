6da3427d3a007c140d86e4edcb809001697f9af8
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-13 10:16:21 +0430

    update(config/Database.ts): add Comment, Message, Setting and SocialNetwork entities

--- config/Database.ts
+++ config/Database.ts
@@ -1,9 +1,12 @@
-import {DataSource} from "typeorm";
-import {AccountModel} from "../src/adapters/out/persistence/models/AccountModel";
-import {FileModel} from "../src/adapters/out/persistence/models/FileModel";
-import {CategoryModel} from "../src/adapters/out/persistence/models/CategoryModel";
-import {PostModel} from "../src/adapters/out/persistence/models/PostModel";
-import {CommentModel} from "../src/adapters/out/persistence/models/CommentModel";
+import { DataSource } from "typeorm";
+import { AccountModel } from "../src/adapters/out/persistence/models/AccountModel";
+import { FileModel } from "../src/adapters/out/persistence/models/FileModel";
+import { CategoryModel } from "../src/adapters/out/persistence/models/CategoryModel";
+import { PostModel } from "../src/adapters/out/persistence/models/PostModel";
+import { CommentModel } from "../src/adapters/out/persistence/models/CommentModel";
+import { MessageModel } from "../src/adapters/out/persistence/models/MessageModel";
+import { SettingModel } from "../src/adapters/out/persistence/models/SettingModel";
+import { SocialNetworkModel } from "../src/adapters/out/persistence/models/SocialNetworkModel";
 
 
 export const db = new DataSource({
@@ -19,7 +22,10 @@ export const db = new DataSource({
         PostModel,
         CategoryModel,
         FileModel,
-        CommentModel
+        CommentModel,
+        MessageModel,
+        SettingModel,
+        SocialNetworkModel
         // path.join(__dirname, "../src/adapters/out/persistence/models/*.ts")
     ]
 })
