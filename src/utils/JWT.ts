8227cfd691b02662a998b5510c4da5a05d4b9227
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-13 10:26:05 +0430

    update(utils/JWT.ts): add JWTResponse type and return null if token is not valid

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -3,18 +3,29 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
+interface JWTResponse {
+    accountId: string,
+    username: string,
+    name: string,
+    family: string,
+    email: string,
+    role: string
+}
 
 export class JWT {
     static generateToken(data: object, expiresIn: string): string {
         return jwt.sign(data, process.env.TOKEN_SECRET_KEY!, {expiresIn: expiresIn})
     }
 
-    static authenticateToken(authHeader: string): JwtPayload | string | null {
+    static authenticateToken(authHeader: string): JWTResponse | null {
         const token = authHeader && authHeader.split(' ')[1]
+
         if (!token) return null;
 
         try {
-            return jwt.verify(token, process.env.TOKEN_SECRET_KEY!);
+            const data = jwt.verify(token, process.env.TOKEN_SECRET_KEY!)
+            if (!data) return null;
+            return data as JWTResponse
         } catch (error) {
             return null
         }
