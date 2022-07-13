00cd06ac5af4a8bb7b7374a58ff4019aaa6dba5e
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-13 10:32:15 +0430

    udpate(app.ts): get server port and root path from config file, add cors option to express, add public directory as static directory

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
 import "reflect-metadata"
 import express from 'express'
+import cors from "cors";
 import bodyParser from 'body-parser';
 import config from 'config'
 import logger from './utils/logger'
 import {router} from "./routes";
 import {db} from "../config/Database";
+import path from "path";
 
 const app = express()
 
+const port = config.get<number>('port')
+const rootPath = config.get<string>('rootPath')
+
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors())
+app.use(express.static(path.join(rootPath, 'public')))
+
 app.use('/api/v1/', router)
 
 // config
-const port = config.get<number>('port')
 
-async function main(){
+async function main() {
     try {
         await db.initialize()
         app.listen(port)
         logger.info(`server is running at http://localhost:${port}`)
-    }catch (error){
+    } catch (error) {
         logger.error(error, 'fail to start server')
     }
 }
